294d9b5572797a7539e9be068fd2178b7f03948c
chuante0607 <cd.liu0607@gmail.com>
2025-10-15 23:14:34 +0800

    Booking UI

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -23,7 +23,7 @@
             const { data } = response;
             movies.value = data.map((item) => ({
                 ...item,
-                path: "/movies/" + item.fileName,
+                path: `/movies/${item.fileName}`,
             }));
         } catch (error) {
             if (error.response) {
